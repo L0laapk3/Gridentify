84d365045fa4e4b72ab6a96d3d7e28e201b6b55a
L0laapk3 <L0laapk3@gmail.com>
2020-05-25 19:42:23 +0200

    added template bot

---
+++ bot.js
@@ -0,0 +1,10 @@
+
+
+
+// This is a template for how to make a bot using this repository.
+// the bot function will be called everytime the board updates.
+// you can call the move function to make a move.
+
+bot = function(board) {
+	// move([board[1][0], board[0][0]]);
+}
